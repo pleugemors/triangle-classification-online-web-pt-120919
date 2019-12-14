fd5e732da48224ace9f8c17f8703cea6269d2473
Paul Leugemors <pleugemors@yahoo.com>
2019-12-14 13:14:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
   def type 
     if side1 == side2 && side1 == side3
       @kind = :equilateral
-    elsif side2 == side3
+    elsif side2 == side3 || side1 == side3 || side1==side2
       @kind = :isosceles
     end
   end
