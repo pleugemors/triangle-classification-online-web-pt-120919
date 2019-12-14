8fb2735f1a642b3ed0f6af0570b8d8a786723882
Paul Leugemors <pleugemors@yahoo.com>
2019-12-14 13:11:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,17 @@
 class Triangle
   # write code here
+ attr_reader :side1, :side2, :side3, :kind
+ 
   def initialize(side1, side2, side3)
-    
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @kind = type
   end
   
-  def kind 
-    
+  def type 
+    if side1 == side2 && side1 == side3
+      @kind = :equilateral
   end
   
 end
