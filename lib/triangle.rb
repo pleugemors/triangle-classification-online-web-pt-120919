33f5a0918d36c507462c9dc7c924ac1d23cc1ee4
Paul Leugemors <pleugemors@yahoo.com>
2019-12-14 13:53:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
       raise TriangleError
     end
     
-    if side1 > side2 + side3 || side2 > side1 + side3 || side3 > side1 + side2 
+    if side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side1 + side2 
       raise TriangleError
     end
     
